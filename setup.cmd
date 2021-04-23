bbc385d4c732a7efb4c5e3ea74444d8f50f1e868
strykerzOfficiel <60830349+strykerzOfficiel@users.noreply.github.com>
2021-04-23 22:34:42 +0200

    Delete setup.cmd

--- setup.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-git config --global user.name "JLStrykerz_2_0"
-git config --global user.email "jlstrykerz_2_0@erinaworld"
-git init
-git remote add origin https://github.com/strykerzOfficiel/TemplateLauncherElectronJS
-pause
