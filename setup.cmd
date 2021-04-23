1fc915dad95a7f8314181eef7f4b0982fa3f0aca
JLStrykerz_2_0 <jlstrykerz_2_0@erinaworld>
2021-04-23 22:20:37 +0200

    New commit

--- setup.cmd
+++ setup.cmd
@@ -2,5 +2,5 @@
 git config --global user.name "JLStrykerz_2_0"
 git config --global user.email "jlstrykerz_2_0@erinaworld"
 git init
-git remote add origin htonliengit https://github.com/strykerzOfficiel/TemplateLauncherElectronJS
+git remote add origin https://github.com/strykerzOfficiel/TemplateLauncherElectronJS
 pause
